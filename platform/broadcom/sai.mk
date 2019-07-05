c2eb6419d6a6b16a6af5c7ddd0fc8c0ef2fdcb63
Prince <prince.sunny@microsoft.com>
2019-07-05 23:12:57 +0000

    Fixed a typo for libsai url

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,5 +1,5 @@
 BRCM_SAI = libsaibcm_4.1.0.1_amd64.deb
-$(BRCM_SAI)_URL = "://sonicstorage.blob.core.windows.net/packages/bcmsai/4.1/libsaibcm_4.1.0.1_amd64.deb?sv=2015-04-05&sr=b&sig=22zOOyyvqri9O95NBKnYSfIMconxCC%2BYyKRy2kD49d0%3D&se=2019-12-01T01%3A22%3A20Z&sp=r"
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.1/libsaibcm_4.1.0.1_amd64.deb?sv=2015-04-05&sr=b&sig=22zOOyyvqri9O95NBKnYSfIMconxCC%2BYyKRy2kD49d0%3D&se=2019-12-01T01%3A22%3A20Z&sp=r"
 
 BRCM_SAI_DEV = libsaibcm-dev_4.1.0.1_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
